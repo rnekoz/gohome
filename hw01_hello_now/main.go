6143ee72c15c6e2fb03a10ee5c65f1f9bbe67f93
rnekoz <rnekoz@akamai.com>
2020-05-18 12:13:02 +0200

    first commit

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/beevik/ntp"
+)
+
+// added for commit.
 func main() {
-	// Place your code here
+	time, _ := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	fmt.Println(time)
 }
